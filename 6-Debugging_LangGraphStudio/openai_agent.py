c43df3f9fb4350caa83fdb27a8313d99706f8383
Jyotirmoy Konwar <jyotirmoykonwarjk@gmail.com>
2025-08-07 13:23:58 +0530

    new

---
+++ 6-Debugging_LangGraphStudio/openai_agent.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langchain_groq import ChatGroq
+from langgraph.graph import END, START
+from langgraph.graph.state import StateGraph
+from langgraph.graph.message import add_message
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+from langchain_core.messages import BaseMessage
+import os
+from dotenv import load_dotenv
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
